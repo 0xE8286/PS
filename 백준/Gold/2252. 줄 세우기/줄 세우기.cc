b44decce0fc676a6b7e0ecb2df353a4e49db7a58
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:38:09 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 4148 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+int small, large;
+int indegree[32001];
+vector<int> edge[32001];
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+	
+	for (int i = 0; i < M; i++) {
+		cin >> small >> large;
+		edge[small].push_back(large);
+		indegree[large]++;
+	}
+	
+	queue<int> q;
+	vector<int> ans;
+
+	for (int i = 1; i <= N; i++) {
+		if (indegree[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		int v = q.front(); q.pop();
+		ans.push_back(v);
+		
+		vector<int> outs = edge[v];
+		for (auto i = outs.begin(); i != outs.end(); i++) {
+			--indegree[*i];
+			if (indegree[*i] == 0) {
+				q.push(*i);
+			}
+		}
+	}
+	
+	for (auto i = ans.begin(); i != ans.end(); i++) {
+		cout << *i << ' ';
+	}
+
+	return 0;
+}
