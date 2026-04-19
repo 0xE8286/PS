53115ee82e426a4295526959b5bce62023ccc0ba
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:57:19 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main() {
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
