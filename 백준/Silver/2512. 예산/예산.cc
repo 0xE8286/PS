809c203517965668cdfac7b006c0b440c6f52325
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:12:51 +0900

    [Silver II] Title: 예산, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.cc
+++ 백준/Silver/2512. 예산/예산.cc
@@ -1,17 +1,17 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int N, M;
-int low, high, result;
 int arr[10000];
 
 int main() {
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios::sync_with_stdio(false);
 
 	cin >> N;
+	
+	int low = 0;
+	int high = 0;
 	
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
@@ -19,6 +19,8 @@ int main() {
 	}
 	cin >> M;
 	
+	int result = 0;
+
 	while (low <= high) {
 		int mid = (low + high) / 2;
 		int sum = 0;
@@ -26,14 +28,13 @@ int main() {
 			sum += min(mid, req);
 		}
 		if (sum <= M) {
-			result = mid;
 			low = mid + 1;
+			result = mid;
 		}
 		else {
 			high = mid - 1;
 		}
 	}
 	cout << result;
-
 	return 0;
 }
