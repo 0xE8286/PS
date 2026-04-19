36caf75fcde977a4f2780b01c58d4158833d6fb2
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:34:26 +0900

    [Gold IV] Title: 친구비, Time: 0 ms, Memory: 2136 KB -BaekjoonHub

---
+++ 백준/Gold/16562. 친구비/친구비.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int N, M, K;
+int v, w;
+int p[10001];
+int cost[10001];
+int min_cost[10001];
+
+int find(int x) {
+	if (p[x] < 0) {
+		return x;
+	} 
+	p[x] = find(p[x]);
+	return p[x];
+}
+
+void merge(int a, int b) {
+	a = find(a);
+	b = find(b);
+	if (a == b) {
+		return;
+	}
+	p[b] = a;
+	min_cost[a] = min(min_cost[a], min_cost[b]);
+}
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+	cin >> N >> M >> K;
+	for (int i = 1; i <= N; i++) {
+		cin >> cost[i];
+		min_cost[i] = cost[i];
+		p[i] = -1;
+	}
+	for (int i = 1; i <= M; i++) {
+		cin >> v >> w;
+		merge(v, w);
+	}
+
+	int sum = 0;
+
+	for (int i = 1; i <= N; i++) {
+		if (p[i] == -1) {
+			sum += min_cost[i];
+		}
+	}
+	if (sum <= K) {
+		cout << sum;
+	}
+	else {
+		cout << "Oh no";
+	}
+
+	return 0;
+}
