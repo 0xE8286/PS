5bf44e29b433e0c19768987ca6a3e1e58bdbdb43
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:22:48 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 448 ms, Memory: 212040 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+string input;
+set<string> str;
+
+int main() {
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	cin >> input;
+	
+	for (int i = 0; i < input.size(); i++) {
+		string s;
+		for (int j = 0; j < input.size() - i; j++) {
+			s.push_back(input[i + j]);
+			str.insert(s);
+		}
+	}
+
+	cout << str.size();
+
+	return 0;
+}
