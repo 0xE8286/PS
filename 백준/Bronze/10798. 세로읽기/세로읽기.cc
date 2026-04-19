867aaeef51cf675cf1c2fc3a4ed19622d851eca8
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:24:02 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+char str[5][16];
+
+int main() {
+	
+	for (int i = 0; i < 5; i++) {
+		cin >> str[i];
+	}
+	
+	for (int i = 0; i < 16; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (str[j][i] != '\0') {
+				cout << str[j][i];
+			}
+		}
+	}
+	return 0;
+}
