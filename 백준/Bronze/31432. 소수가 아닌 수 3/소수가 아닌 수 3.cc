45e241d407cdd85a8002fc30844c7d14b3e9f4ed
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:26:38 +0900

    [Bronze I] Title: 소수가 아닌 수 3, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31432. 소수가 아닌 수 3/소수가 아닌 수 3.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   cin.tie(NULL)->sync_with_stdio(false);
+
+   int N, d;
+   cin >> N >> d;
+   cout << "YES" << '\n';
+   cout << d * 1111;
+   
+   return 0;
+}
