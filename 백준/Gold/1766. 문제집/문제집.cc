d33917c8c9050ac3102d2bb2bee27465688e2616
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:34:34 +0900

    [Gold II] Title: 문제집, Time: 32 ms, Memory: 4144 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, M;
+int pre, nxt;
+int indegree[32001];
+vector<int> edge[32001];
+vector<int> answer;
+
+int main() {
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> pre >> nxt;
+		indegree[nxt]++;
+		edge[pre].push_back(nxt);
+	}
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+	
+	for (int i = 1; i <= N; i++) {
+		if (indegree[i] == 0) {
+			pq.push(i);
+		}
+	}
+	
+	while (!pq.empty()) {
+		int curr = pq.top(); pq.pop();
+		answer.push_back(curr);
+
+		for (int next : edge[curr]) {
+			if (--indegree[next] == 0) {
+				pq.push(next);
+			}
+		}
+	}
+
+	for (int order : answer) {
+		cout << order << ' ';
+	}
+
+	return 0;
+}
