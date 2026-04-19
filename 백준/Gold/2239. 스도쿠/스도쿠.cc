4cc0b857f2acd8d0e5fba882cd15200ff599f561
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:30:26 +0900

    [Gold IV] Title: 스도쿠, Time: 360 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.cc
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+char cudoku[9][10];
+int sudoku[9][9];
+int blank[81][2];
+int cnt;
+bool complete = false;
+
+bool isValid(int x, int y, int ans) {
+	for (int i = 0; i < 9; i++) {
+		if (sudoku[x][i] == ans || sudoku[i][y] == ans)
+			return false;
+	}
+
+	if (x < 3) {
+		for (int i = 0; i < 3; i++) {
+			if (y < 3) {
+				for (int j = 0; j < 3; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+			else if (y < 6) {
+				for (int j = 3; j < 6; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+			else {
+				for (int j = 6; j < 9; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+		}
+	}
+	else if (x < 6) {
+		for (int i = 3; i < 6; i++) {
+			if (y < 3) {
+				for (int j = 0; j < 3; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+			else if (y < 6) {
+				for (int j = 3; j < 6; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+			else {
+				for (int j = 6; j < 9; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+		}
+	}
+	else {
+		for (int i = 6; i < 9; i++) {
+			if (y < 3) {
+				for (int j = 0; j < 3; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+			else if (y < 6) {
+				for (int j = 3; j < 6; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+			else {
+				for (int j = 6; j < 9; j++)
+					if (sudoku[i][j] == ans)
+						return false;
+			}
+		}
+	}
+	return true;
+}
+
+void dfs(int b) {
+	if (b == cnt) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				cout << sudoku[i][j];
+			}
+			cout << '\n';
+		}
+		complete = true;
+		return;
+	}
+	for (int i = 1; i <= 9; i++) {
+		int x = blank[b][0];
+		int y = blank[b][1];
+
+		if (isValid(x, y, i)) {
+			sudoku[x][y] = i;
+			dfs(b + 1);
+			if (complete) {
+				return;
+			}
+			sudoku[x][y] = 0;
+		}
+	}
+}
+
+int main(void) {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	for (int i = 0; i < 9; i++) {
+		cin >> cudoku[i];
+		for (int j = 0; j < 9; j++) {
+			sudoku[i][j] = cudoku[i][j] - '0';
+			if (sudoku[i][j] == 0) {
+				blank[cnt][0] = i;
+				blank[cnt++][1] = j;
+			}
+		}
+	}
+	dfs(0);
+	return 0;
+}
