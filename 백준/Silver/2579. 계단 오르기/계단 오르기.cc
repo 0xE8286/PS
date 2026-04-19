067fd21cd9ec4c76187b9d88ae8559ed3750e34d
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:08:21 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,25 +2,24 @@
 #include <cmath>
 using namespace std;
 
-int N, sum;
+int N;
 int stair[301];
-int score[301];
+int score[301][3];
 
 int main() {
 
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
 		cin >> stair[i];
-		sum += stair[i];
-	}
-	score[1] = stair[1];
-	score[2] = stair[2];
-	score[3] = stair[3];
-
-	for (int i = 4; i <= N; i++) {
-		score[i] = min(score[i - 3], score[i - 2]) + stair[i];
 	}
 
-	cout << sum - min(score[N-1], score[N-2]) <<  endl;
+	score[1][1] = stair[1];
+	score[1][2] = stair[1];
+
+	for (int i = 2; i <= N; i++) {
+		score[i][1] = max(score[i - 2][1], score[i - 2][2]) + stair[i];
+		score[i][2] = score[i - 1][1] + stair[i];
+	}
+	cout << max(score[N][1], score[N][2]);
 	return 0;
 }
