03f5da4fc328852fc37df78c90e94792e3c3226f
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:41:18 +0900

    [Silver III] Title: N과 M (7), Time: 496 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int num[7];
+vector<int> v;
+
+void dfs(int d) {
+	if (d == M) {
+		for (auto i = v.begin(); i != v.end(); i++) {
+			cout << *i << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		v.push_back(num[i]);
+		dfs(d + 1);
+		v.pop_back();
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin >> N >> M;
+	
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+	}
+	sort(num, &num[N]);
+	dfs(0);
+	
+	return 0;
+}
