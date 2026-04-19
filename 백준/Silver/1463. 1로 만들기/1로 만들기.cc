34998b362862d139f731a3f1b51331fd8f77e074
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:58:23 +0900

    [Silver III] Title: 1로 만들기, Time: 40 ms, Memory: 52640 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,27 +1,45 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-int N;
-int dp[1000001];
+int data[1000000];
 
-int main() {
 
+int makeOne(int n){
+	if (n == 1)	return 0;
+	if (data[n] != 0) return data[n];
+
+	int sol_1 = 2147483647;
+	int sol_2 = 2147483647;
+	int sol_3 = 2147483647;
+
+	sol_1 = makeOne(n - 1) + 1;
+	if (n % 2 == 0)
+		sol_2 = makeOne(n / 2) + 1;
+	
+	if (n % 3 == 0)
+		sol_3 = makeOne(n / 3) + 1;
+	
+	if (sol_1 < sol_2){
+		if (sol_1 < sol_3)
+			data[n] = sol_1;
+		else
+			data[n] = sol_3;
+	}
+	else {
+		if (sol_2 < sol_3)
+			data[n] = sol_2;
+		else
+			data[n] = sol_3;
+	}
+	return data[n];
+}
+
+int main(){
+	int N; 
 	cin >> N;
 
-	dp[1] = 0;
-	dp[2] = 1;
-	dp[3] = 1;
-
-	for (int i = 4; i <= N; i++) {
-		int a = 99999999;
-		if (i % 3 == 0 && dp[i / 3] + 1 < a) a = dp[i / 3] + 1;
-		if (i % 2 == 0 && dp[i / 2] + 1 < a) a = dp[i / 2] + 1;
-		if (dp[i - 1] + 1 < a) a = dp[i - 1] + 1;
-		dp[i] = a;
-	}
-
-	cout << dp[N];
+	makeOne(N);
+	cout << data[N];
 
 	return 0;
 }
