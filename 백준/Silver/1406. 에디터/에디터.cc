3c3c1b82229d7c31a4507767f2e2bf0d1f2f9ea6
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:52:07 +0900

    [Silver II] Title: 에디터, Time: 68 ms, Memory: 21020 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+string str;
+list<char> editor;
+int N;
+
+void print() {
+	for (auto i = editor.begin(); i != editor.end(); i++) {
+		cout << *i;
+	}
+	cout << '\n';
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> str;
+	for (int i = 0; i < str.size(); i++) {
+		editor.push_back(str[i]);
+	}
+	
+	auto cursor = editor.end();
+	
+	cin >> N;
+
+	char cmd, param;
+
+	for (int i = 0; i < N; i++) {
+		cin >> cmd;
+		if (cmd == 'L') {
+			if (cursor == editor.begin()) continue;
+			cursor--;
+		}
+		else if (cmd == 'D') {
+			if (cursor == editor.end()) continue;
+			cursor++;
+		}
+		else if (cmd == 'B') {
+			if (cursor == editor.begin()) continue;
+			cursor--;
+			cursor = editor.erase(cursor);
+		}
+		else if (cmd == 'P') {
+			cin >> param;
+			editor.insert(cursor, param);
+		}
+	}
+
+	print();
+
+	return 0;
+}
