17bd51687a6918f7eaebea1fab066cead9a0ed57
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:07:54 +0900

    [Silver I] Title: 카잉 달력, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	while (b != 0) {
+		int mod = a % b;
+		a = b;
+		b = mod;
+	}
+	return a;
+}
+
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
+
+
+int main() {
+
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int T;
+	int M, N, X, Y;
+	cin >> T;
+	for (int tc = 0; tc < T; tc++) {
+		cin >> M >> N >> X >> Y;
+		
+		X = X - 1;
+		Y = Y - 1;
+		
+		bool found = false;
+		int i = 0;
+	
+		for (i = X; i <= (N * M); i += M) {
+			if (i % N == Y) {
+				found = true;
+				break;
+			}
+		}
+
+		if (found) {
+			cout << i + 1 << '\n';
+		}
+		else {
+			cout << -1 << '\n';
+		}
+	}
+
+	return 0;
+}
