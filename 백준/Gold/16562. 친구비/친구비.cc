a4c9e7303c6db25d340cd615c26de60d5efc1553
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:34:30 +0900

    [Gold IV] Title: 친구비, Time: 8 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.cc
+++ 백준/Gold/16562. 친구비/친구비.cc
@@ -28,9 +28,6 @@ void merge(int a, int b) {
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
 
 	cin >> N >> M >> K;
 	for (int i = 1; i <= N; i++) {
