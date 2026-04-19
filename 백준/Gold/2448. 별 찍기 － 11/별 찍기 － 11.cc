4cf09fb2043824fa920bee33ed3fe71bb8e7d04a
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:24:21 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 904 ms, Memory: 20460 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
@@ -8,18 +8,19 @@ void star(int y, int x, int N) {
 
 	if (N == 3) {
 		a[y][x] = true;
-
+        
 		a[y + 1][x - 1] = true; 
-		a[y + 1][x + 1] = true;
-
-		a[y + 2][x - 2] = true; 
-		a[y + 2][x - 1] = true; 
-		a[y + 2][x] = true;
-		a[y + 2][x + 1] = true;
-		a[y + 2][x + 2] = true;
-
-		return;
+        a[y + 1][x + 1] = true;
+		
+        a[y + 2][x - 2] = true; 
+        a[y + 2][x - 1] = true; 
+        a[y + 2][x] = true;
+		a[y + 2][x + 1] = true; 
+        a[y + 2][x + 2] = true;
+        
+        return;
 	}
+    
 	N /= 2;
 	star(y, x, N);
 	star(y + N, x - N, N);
@@ -28,10 +29,6 @@ void star(int y, int x, int N) {
 
 int main() {
 
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios::sync_with_stdio(false);
-
 	int N;
 	cin >> N;
 	
@@ -39,6 +36,7 @@ int main() {
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N * 2; j++) {
+			
 			if (a[i][j] == true)
 				cout << '*';
 			if (a[i][j] == false)
