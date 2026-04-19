9842964f5110aef7a260768fbe91571d7dec17de
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:37:44 +0900

    [Gold III] Title: 게임 개발, Time: 4 ms, Memory: 2440 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N;
+
+int cost[501];
+
+int indegree[501];
+vector<int> outEdge[501];
+int sum[501];
+
+int main() {
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	queue<int> q;
+	int base;
+
+	cin >> N;
+	
+	for (int v = 1; v <= N; v++) {
+		cin >> cost[v];
+		cin >> base;
+		if (base == -1) {
+			sum[v] = cost[v];
+			q.push(v);
+			continue;
+		}
+		while (base != -1) {
+			indegree[v]++;
+			outEdge[base].push_back(v);
+			cin >> base;
+		}
+	}
+
+	while (!q.empty()) {
+		int v = q.front(); q.pop();
+		
+		for (auto i : outEdge[v]) {
+			indegree[i]--;
+			sum[i] = max(sum[i], sum[v] + cost[i]);
+			if (indegree[i] == 0) {	
+				q.push(i);
+			}
+		}
+	}
+	
+	for (int i = 1; i <= N; i++) {
+		cout << sum[i] << '\n';
+	}
+
+	return 0;
+}
