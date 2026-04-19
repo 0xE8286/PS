e9a50abfed19e1d5229f81351efd0c83f99f1245
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:33:28 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 1001
+int N;
+int tile[MAX_N];
+
+int main(void) {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+	tile[1] = 1;
+	tile[2] = 3;
+
+	for (int i = 3; i <= N; i++) {
+		tile[i] = ((tile[i - 2] * 2) + tile[i - 1]) % 10007;
+	}
+	cout << tile[N];
+
+	return 0;
+}
