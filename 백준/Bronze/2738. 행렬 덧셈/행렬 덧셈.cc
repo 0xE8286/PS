2eac751c97a8ac53414e1357f7779025026a4446
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:40:47 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int a[100][100];
+int b[100][100];
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> a[i][j];
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> b[i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << a[i][j] + b[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
