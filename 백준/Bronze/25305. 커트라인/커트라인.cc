7212bd010071cb716929c50a2f92878eacfecdc0
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:21:44 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, K;
+int arr[1000];
+int main() {
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N, greater<>());
+	cout << arr[K-1];
+	return 0;
+}
