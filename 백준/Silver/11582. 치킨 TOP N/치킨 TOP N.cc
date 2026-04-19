b4ecf93395926f67307fd6868248733a60fb7691
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:24:01 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 284 ms, Memory: 10212 KB -BaekjoonHub

---
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int N, K;
+int arr[1048576];
+int temp[1048576];
+
+void sort(int start, int mid, int end) {
+	int left = start;
+	int right = mid + 1;
+	int temp_index = start;
+
+	while (left <= mid && right <= end) {
+		if (arr[left] <= arr[right]) {
+			temp[temp_index++] = arr[left++];
+		}
+		else {
+			temp[temp_index++] = arr[right++];
+		}
+	}
+	
+	while (left <= mid) {
+		temp[temp_index++] = arr[left++];
+	}
+	while (right <= end) {
+		temp[temp_index++] = arr[right++];
+	}
+
+	for (int i = start; i <= end; i++) {
+		arr[i] = temp[i];
+	}
+}
+
+void merge(int start, int end, int cnt) {
+	if (start >= end) return;
+
+	int mid = (start + end) / 2;
+
+	merge(start, mid, cnt/2);
+	merge(mid + 1, end, cnt/2);
+
+	if (cnt <= N / K) {
+		sort(start, mid, end);
+	}
+}
+
+int main() {
+
+	cout.tie(NULL);
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	cin >> K;
+
+	merge(0, N - 1, N);
+
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << ' ';
+	}
+}
