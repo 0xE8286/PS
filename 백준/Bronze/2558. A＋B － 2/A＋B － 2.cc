0fe54a000d535ca3d440b975721f42c6c3cdeffe
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:38:59 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+	cout << N + M;
+	
+	return 0;
+}
