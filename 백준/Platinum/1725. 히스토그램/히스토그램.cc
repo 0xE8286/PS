edd6909075b21a293a75df7eebeda576ea078255
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:59:36 +0900

    [Platinum V] Title: 히스토그램, Time: 8 ms, Memory: 3072 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.cc
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -1,50 +1,59 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
+int hist[100002];
 int N;
-long long h[100001];
-
-long long getSize(long long s, long long e) {
-	if (s == e) return 0;
-	if (s + 1 == e) return h[s];
-
-	long long mid = (s + e) / 2;
-	long long size = max(getSize(s, mid), getSize(mid, e));
-	
-	long long l = mid;
-	long long r = mid;
-
-	long long width = 1;
-	long long height = h[mid];
-
-	while ((r - l + 1) < e - s) {
-		long long lh = (l > s) ? min(height, h[l - 1]) : -1;
-		long long rh = (l < e - 1) ?  min(height, h[r + 1]) : -1;
-		if (lh >= rh) {
-			height = lh;
-			l--;
-		}
-		else {
-			height = rh;
-			r++;
-		}
-		size = max(size, ++width * height);
-	}
-	return size;
-}
+struct bar {
+	int height;
+	int dist;
+};
+stack<bar> s;
 
 int main() {
 
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
-
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> h[i];
-	}
-	cout << getSize(0, N) << '\n';
 	
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> hist[i];
+	}
+
+	int max_width = 0;
+
+	for (int i = 1; i <= N; i++) {
+		while (!s.empty() && s.top().height >= hist[i]) {
+			bar now = s.top(); s.pop();
+			if (!s.empty()) {
+				bar prev = s.top();
+				max_width = max(max_width, now.height * ((i - 1) - prev.dist));
+			}
+			else {
+				max_width = max(max_width, now.height * (i - 1));
+			}
+		}
+
+		bar now;
+		now.height = hist[i];
+		now.dist = i;
+		
+		s.push(now);
+	}
+
+	while (!s.empty()) {
+		bar now = s.top(); s.pop();
+		if (!s.empty()) {
+			bar prev = s.top();
+			max_width = max(max_width, now.height * (N - prev.dist));
+		}
+		else {
+			max_width = max(max_width, now.height * N);
+		}
+	}
+
+	cout << max_width;
 
 	return 0;
 }
