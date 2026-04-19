533dfa4172a39445585d06da2493b6947ed79ead
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:53:23 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int a, b, c;
+
+	cin >> a >> b >> c;
+	
+	if (b >= c) {
+		cout << -1;
+		return 0;
+	}
+	
+	cout << a/(c-b) + 1 ;
+
+	return 0;
+}
