f99fb2df5387e2cdc933c5084fc1627918b51e2b
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:43:19 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int ans[9];
+
+void dfs(int cnt, int num) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			cout << ans[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = num; i <= N; i++) {
+		ans[cnt] = i;
+		dfs(cnt + 1, i);
+	}
+}
+
+int main(void) {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	cin >> N >> M;
+
+	dfs(0, 1);
+
+	return 0;
+}
