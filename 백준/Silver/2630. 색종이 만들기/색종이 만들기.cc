07047fd11909d8c31f5e32a355ae96a8e531b5e8
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:30:07 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int paper[129][129];
+int blue, white;
+
+bool is_one_color(int y_start, int x_start, int y_end, int x_end) {
+	int mark = paper[y_start][x_start];
+	for (int i = y_start; i <= y_end; i++) {
+		for (int j = x_start; j <= x_end; j++) {
+			if (paper[i][j] != mark) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+
+void cut(int y_start, int x_start, int y_end, int x_end) {
+	if (!is_one_color(y_start, x_start, y_end, x_end)) {
+		int y_mid = (y_start + y_end) / 2;
+		int x_mid = (x_start + x_end) / 2;
+		
+		cut(y_start, x_start, y_mid, x_mid);
+		cut(y_start, x_mid + 1, y_mid, x_end);
+		cut(y_mid + 1, x_start, y_end, x_mid);
+		cut(y_mid + 1, x_mid + 1, y_end, x_end);
+	}
+	else {
+		if (paper[y_start][x_start] == 1)
+			blue++;
+		else
+			white++;
+	}
+}
+
+int main(void) {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> paper[i][j];
+		}
+	}
+	cut(0, 0, N - 1, N - 1);
+	cout << white << '\n' << blue ;
+
+	return 0;
+}
