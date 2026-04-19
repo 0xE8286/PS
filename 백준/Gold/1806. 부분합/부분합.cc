d108e72b42dbc9eb2879edd20aee8a68c0f52c78
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:29:04 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int N, S;
+int numbers[100000];
+int min_length = 100001;
+
+int main() {
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> S;
+
+	for (int i = 0; i < N; i++) {
+		cin >> numbers[i];
+	}
+
+	int start = 0;
+	int end = 0;
+	int sum = numbers[start];
+
+	while (end < N) {
+		if (sum >= S) {
+			int len = end - start + 1;
+			if (len < min_length) {
+				min_length = len;
+			}
+			sum -= numbers[start];
+			start++;
+		}
+		else {
+			if (++end == N) break;
+			sum += numbers[end];
+		}
+	}
+
+	if (min_length == 100001) {
+		cout << 0;
+	}
+	else {
+		cout << min_length;
+	}
+	
+	return 0;
+}
