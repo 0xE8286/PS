2c914c4765ff6e65e4d34b3b6bda437b989f9a97
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:57:40 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,25 +1,41 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int N;
-int stair[301];
-int score[301][3];
+int point[301];
+long long dp[301][2];
+
+
+long long max(long long a, long long b) {
+	if (a > b)
+		return a;
+	else 
+		return b;
+}
 
 int main() {
 
 	cin >> N;
+
 	for (int i = 1; i <= N; i++) {
-		cin >> stair[i];
+		cin >> point[i];
 	}
 
-	score[1][1] = stair[1];
-	score[1][2] = stair[1];
+	//시작점이 0번 인덱스
 
-	for (int i = 2; i <= N; i++) {
-		score[i][1] = max(score[i - 2][1], score[i - 2][2]) + stair[i];
-		score[i][2] = score[i - 1][1] + stair[i];
+	dp[0][0] = point[1]; 
+	dp[0][1] = point[2]; 
+
+	dp[1][0] = point[1] + point[2];
+	dp[1][1] = point[1] + point[3];
+ 
+	for (int i = 2; i < N; i++) { //N-1 층의 0번 인덱스 까지만 채우묜 댐
+		dp[i][0] = dp[i - 2][1] + point[i + 1];
+		if (i == N - 1) break;
+		dp[i][1] = max(dp[i - 1][0], dp[i-2][1]) + point[i + 2];
 	}
-	cout << max(score[N][1], score[N][2]);
+
+	cout << max(dp[N - 1][0], dp[N - 2][1]);
+	   	 
 	return 0;
 }
