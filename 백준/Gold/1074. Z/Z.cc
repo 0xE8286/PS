ea3061862edd9f5f074e76f2a9e85652ee2bd913
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:33:53 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -1,33 +1,37 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int N;
-int R, C;
+int r, c;
 int cnt;
 int result;
 
+int pow(int x, int n) {
+	if (n == 1) return x;
+	return x * pow(x, n - 1);
+}
+
 void z(int y, int x, int n) {
-	if (y == R && x == C) {
+	if (r == y && c == x) {
 		result = cnt;
 		return;
 	}
-	if (y > R || y + n <= R || x > C || x + n <= C) {
+	if (r < y || r >= y+n || c < x || c >= x+n) {
 		cnt += n * n;
 		return;
 	}
-	int half = n / 2;
-	z(y, x, half);
-	z(y, x + half, half);
-	z(y + half, x, half);
-	z(y + half, x + half, half);
+	int piece = n / 2;
+	z(y, x, piece);
+	z(y, x + piece, piece);
+	z(y + piece, x, piece);
+	z(y + piece, x + piece, piece);
 }
 
 int main(void) {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	cin >> N >> R >> C;
+	cin >> N >> r >> c;
 	z(0, 0, pow(2, N));
 	cout << result;
 
