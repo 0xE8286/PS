204831b6bde7eb8329aea513fd7dfdc03523685f
0xE8286 <0xe8286@gmail.com>
2026-04-19 19:35:35 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
+	int sum = 0;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> n;
+		sum += n * n;
+	}
+
+	cout << sum % 10;
+
+	return 0;
+}
