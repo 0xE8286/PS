5fc258ef68ce82a801a05434d85f4a6e7911f289
0xE8286 <0xe8286@gmail.com>
2026-04-19 18:15:35 +0900

    [Gold IV] Title: 어떤 우유의 배달목록 (Easy), Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/23835. 어떤 우유의 배달목록 （Easy）/어떤 우유의 배달목록 （Easy）.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, Q;
+vector<int> graph[1001];
+int milk[1001];
+bool visit[1001];
+
+int deliver(int curr, int end, int cnt) {
+	if (curr == end) {
+		return cnt;
+	}
+	for (int next : graph[curr]) {
+		if (visit[next] == 0) {
+			visit[next] = 1;
+			
+			int result = deliver(next, end, cnt + 1);
+			milk[next] += result;
+			
+			if (result > 0) {
+				return result - 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int main() {
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+	int a, b;
+
+	for (int i = 0; i < N - 1; i++) {
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	
+	cin >> Q;
+	int cmd, x;
+	for (int i = 0; i < Q; i++) {
+		cin >> cmd;
+		if (cmd == 1) {
+			cin >> a >> b;
+			
+			fill(visit, visit + N + 1, false);
+			visit[a] = true;
+
+			deliver(a, b, 0);
+		}
+		else {
+			cin >> x;
+			cout << milk[x] << '\n';
+		}
+	}
+
+	return 0;
+}
